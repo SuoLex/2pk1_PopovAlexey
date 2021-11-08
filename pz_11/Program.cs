50a550f2fb3335adf5d3b1524a480bf20ef35b1c
Lexa Popov <papov97@inbox.ru>
2021-11-08 10:47:37 +0300

    pz_11

--- pz_11/Program.cs
+++ pz_11/Program.cs
@@ -6,7 +6,37 @@ namespace pz_11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите 1-ю строку: ");
+            string a1 =Console.ReadLine();
+            Console.Write("Введите 2-ю строку: ");
+            string a2 = Console.ReadLine();
+            Console.Write("Введите 3-ю строку: ");
+            string a3 = Console.ReadLine();
+            Console.Write("Введите 4-ю строку: ");
+            string a4 = Console.ReadLine();
+            int b = 0;
+            int b1 = 0;
+            int b2 = 0;
+            b = Math.Max(a1.Length, a2.Length);
+            b1 = Math.Max(a3.Length, a4.Length);
+            b2 = Math.Max(b, b1);
+            if (b2 == a1.Length)
+            {
+                Console.WriteLine("1");
+            }
+            if (b2 == a2.Length)
+            {
+                Console.WriteLine("1");
+            }
+            if (b2 == a3.Length)
+            {
+                Console.WriteLine("3");
+            }
+            if (b2 == a4.Length)
+            {
+                Console.WriteLine("4");
+            }
+
         }
     }
 }
