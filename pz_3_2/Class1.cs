39180cbab83a2ef2133ba728d8aaecc9eab1a78e
Lexa Popov <popov_infotmatika@mail.ru>
2022-02-23 15:15:43 +0500

    b

--- pz_3_2/Class1.cs
+++ pz_3_2/Class1.cs
@@ -14,16 +14,16 @@ namespace pz_3._2
 {
     public class ParkingTicket
     {
-
-
+        static int All_sum = 0;                                             //два статика, потому что попросили (общая сумма талонов и общая сумма всех талонов)
+        static int All_price = 0;
         private int id = 1;
         private DateTime START = DateTime.UtcNow;
         private DateTime END = DateTime.Now;
-        static int price; // сумма парковки
-        public float summ;                              //поля и переменные 
-        public string carID;                            
-        public TimeSpan interval; //ИНТЕРВАЛ ДЛЯ ТОГО, ЧТОБЫ СЧИТАТЬ СУММУ ПАРКОВКИ
-        private int ID 
+        public int price;                                               //Сумма парковки
+        public float summ;                                              //поля и переменные 
+        public string carID;
+        public TimeSpan interval;                                       //ИНТЕРВАЛ ДЛЯ ТОГО, ЧТОБЫ СЧИТАТЬ СУММУ ПАРКОВКИ
+        private int ID
         {
             set
             {
@@ -33,19 +33,19 @@ namespace pz_3._2
                 }
                 else { id = value; }
             }
-            get { return id;}
+            get { return id; }
         }
         private DateTime startTime
         {
             set
             {
-                if (value>endTime)
+                if (value > endTime)
                 {
                     Console.WriteLine("\nВремя начала парковки строго меньше времени окончания");
                 }
                 else { START = value; }
             }
-            get 
+            get
             {
                 return START;
             }
@@ -65,16 +65,13 @@ namespace pz_3._2
                 return END;
             }
         }
-
-       
-
-
+        //------------------------------------------------------------------------------------------------------------------------
         public ParkingTicket()
         {         
             ID = 0; 
             summ = 0;
             carID = "НЕИЗВЕСТНО";
-            startTime = DateTime.UtcNow;
+            startTime = DateTime.UtcNow;                                //Если значение пользователе мне будте присвоено
             endTime = DateTime.Now;
             interval = endTime - startTime;
             price = 0;
@@ -83,30 +80,30 @@ namespace pz_3._2
         public ParkingTicket(int ID)
         {
             this.ID = ID;
-        }
+        }                                 //Варианты с различными введенными значениями
         public ParkingTicket(float summ)
         {
             this.summ = summ;
-        }
+        }                             //Варианты с различными введенными значениями
         public ParkingTicket(string carID)
         {
             this.carID = carID;
-        }
+        }                           //Варианты с различными введенными значениями
         public ParkingTicket(int ID, float summ)
         {
             this.ID = ID;
             this.summ = summ;
-        }
+        }                     //Варианты с различными введенными значениями
         public ParkingTicket(int ID, string carID)
         {
             this.ID = ID;
             this.carID = carID;
-        }
+        }                   //Варианты с различными введенными значениями
         public ParkingTicket(float summ, string carID)
         {
             this.summ = summ;
             this.carID = carID;
-        }
+        }               //Варианты с различными введенными значениями
         public ParkingTicket(int ID, float summ, string carID)
         {
             this.ID = ID;
@@ -117,16 +114,21 @@ namespace pz_3._2
             interval = endTime - startTime;
             price = 50 * interval.Hours;
 
-        }
-
-        public void GetTicketInfo()
+        }       //Варианты с различными введенными значениями
+        public void GetTicketInfo()                                     //Классный метод с выводом описания текущего объекта
         {
-            Console.WriteLine($"\nВаш уникальный номер талона:{ID}\nСтоимость парковки:{summ}\nВремя начала парковки:{startTime}\nВремя окончания парковки:{endTime}\nНомер авто:{carID}\nВаша цена за парковку:{price} рублей\n");
-            
-        }
+            All_sum += 1;
+            All_price += price;
+            Console.WriteLine($"\nВаш уникальный номер талона:{ID}\n" +
+                $"Стоимость парковки:{summ}\nВремя начала парковки:{startTime}\n" +
+                $"Время окончания парковки:{endTime}\nНомер авто:{carID}\n" +
+                $"Ваша цена за парковку:{price} рублей\n" +
+                $"\n Общая сумма талонов:{All_sum}\n" +
+                $"Общая стоимость всех талонов:{All_price}");
 
+        }
     }
-    public class Pizza
+    public class Pizza                                                  //Вспомогательный класс, который помогает обозначить какие данные были введены
     {
         public Pizza(int ID, float summ, string carID)
         {
